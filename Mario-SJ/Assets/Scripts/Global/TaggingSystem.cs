b989612657f2718fe9d3e33516c9365ce861dd43
Suyash Lal <suyash.a.lal@gmail.com>
2023-10-03 02:25:06 +0530

    tilesmapping
    i did a basic background with tilemapping, i need to figure out how to individually script tiles now. since it was late i stopped but it seems interesting enough to solve

---
+++ Mario-SJ/Assets/Scripts/Global/TaggingSystem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Tag", menuName = "Tags/New Tag")]      // this is what we will see if we right click on the objects in unity
+public class TilesScripts : ScriptableObject                            // allow it to change tile behavior
+{
+    public string Name => name;
+}
